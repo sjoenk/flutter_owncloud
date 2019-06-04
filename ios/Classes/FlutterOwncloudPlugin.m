7157484c922a48558b1f73732dcad3568e4ab695
Paul Schunck <p.schunck@silversoft.nl>
2019-06-04 16:01:24 +0200

    Initial commit

---
+++ ios/Classes/FlutterOwncloudPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterOwncloudPlugin.h"
+
+@implementation FlutterOwncloudPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_owncloud"
+            binaryMessenger:[registrar messenger]];
+  FlutterOwncloudPlugin* instance = [[FlutterOwncloudPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
